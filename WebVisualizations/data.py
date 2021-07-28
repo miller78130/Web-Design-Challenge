76d6427227ee56cbe3088e60ac13bd4579e59c15
miller78130 <miller78130@gmail.com>
2021-07-27 19:34:56 -0500

    final

--- WebVisualizations/data.py
+++ WebVisualizations/data.py
@@ -1,8 +1,8 @@
 import pandas as pd
 #read in Data
-data=pd.read_csv("Resources/cities.csv")
+data=pd.read_csv("WebVisualizations/Resources/cities.csv")
 
-weather_data = data.set_index("City_ID")
+weather_data = data.set_index("City")
 
 #convert data to html
-weather_data.to_html('data.html')
+weather_data.to_html('raw_data.html')
